090dfb367a8f445670501e319fb226df803f1887
福島聡 <satoshi9515@192.168.0.5>
2023-07-30 16:21:36 +0900

    環境情報を再設定

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Throwable; //追加
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -34,7 +35,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)  //修正
     {
         parent::report($exception);
     }
@@ -48,7 +49,7 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)  //修正
     {
         return parent::render($request, $exception);
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,17 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        //check that app is local
+        if (!$this->app->isLocal()) {
+            //else register your services you require for production
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
+        if ($this->app->isLocal()) {
+            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+        } else {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
